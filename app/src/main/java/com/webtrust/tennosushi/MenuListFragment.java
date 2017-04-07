9fc83d90376ec1b56b9cd51c615e73b388daf13a
Писарев Юрий <rarescrap@gmail.com>
2017-04-07 16:35:46 +0800

    Добавлен набросок алерта, возникающий при отсуствии подключения

--- app/src/main/java/com/webtrust/tennosushi/MenuListFragment.java
+++ app/src/main/java/com/webtrust/tennosushi/MenuListFragment.java
@@ -1,11 +1,13 @@
 package com.webtrust.tennosushi;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -98,12 +100,33 @@ public class MenuListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true); // у фрагмента имеются команды меню
+        View returnedView; // Возвращаемый View
 
         // Inflate the layout for this fragment
         if (currentMode == CARD_MODE) {
-            return inflater.inflate(R.layout.fragment_menu_card_list, container, false);
+            returnedView = inflater.inflate(R.layout.fragment_menu_card_list, container, false);
+            try {
+                URL url = new URL("http://192.168.1.254/index.php");
+
+                GetDataTask getLocalDataTask = new GetDataTask();
+                getLocalDataTask.execute(url);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            return returnedView;
         }else { // currentMode == PLATE_MODE
-            return inflater.inflate(R.layout.fragment_menu_plates_list, container, false);
+            returnedView = inflater.inflate(R.layout.fragment_menu_plates_list, container, false);
+            try {
+                URL url = new URL("http://192.168.1.254/index.php");
+
+                GetDataTask getLocalDataTask = new GetDataTask();
+                getLocalDataTask.execute(url);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            return returnedView;
         }
     }
 
@@ -120,16 +143,6 @@ public class MenuListFragment extends Fragment {
         }else { // currentMode == PLATE_MODE
             menuItemListGridView = (GridView) getView().findViewById(R.id.platesList);
             menuItemListGridView.setAdapter(menuItemArrayAdapter);
-        }
-
-        try {
-            URL url = new URL("http://192.168.1.254/index.php");
-
-            GetDataTask getLocalDataTask = new GetDataTask();
-            getLocalDataTask.execute(url);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -233,6 +246,7 @@ public class MenuListFragment extends Fragment {
      * @author RareScrap
      */
     private class GetDataTask extends AsyncTask<URL, Void, JSONObject> {
+        public int CONNECTION_TIMEOUT = 5000; // Максимальное время ожидания данных
         /**
          * Получение данных из сети
          * @param params URL для получения JSON файла
@@ -244,7 +258,10 @@ public class MenuListFragment extends Fragment {
 
             try {
                 connection = (HttpURLConnection) params[0].openConnection(); // Для выдачи запроса достаточно открыть объект подключения
+                connection.setConnectTimeout(this.CONNECTION_TIMEOUT);
                 int response = connection.getResponseCode(); // Получить код ответа от веб-сервера
+
+                //response = 404; // Это тест
 
                 if (response == HttpURLConnection.HTTP_OK) {
                     StringBuilder builder = new StringBuilder();
@@ -260,8 +277,7 @@ public class MenuListFragment extends Fragment {
                     }
 
                     return new JSONObject(builder.toString());
-                }else {
-                }
+                }else {}
             }
             catch (Exception e) {
                 e.printStackTrace();
@@ -289,6 +305,22 @@ public class MenuListFragment extends Fragment {
                 }else { // currentMode == PLATE_MODE
                     menuItemListGridView.smoothScrollToPosition(0);
                 }
+            } else { // Вывод алерта в случае, если данные не дошли
+                AlertDialog.Builder adBuilder = new AlertDialog.Builder(getActivity());
+
+                // Назначить сообщение AlertDialog
+                adBuilder.setMessage(R.string.noConnection_useCashe);
+
+                // Добавить кнопку OK в диалоговое окно
+                adBuilder.setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {}
+                        }
+                );
+
+                // Отображение диалогового окна
+                adBuilder.create().show();
             }
         }
     }
