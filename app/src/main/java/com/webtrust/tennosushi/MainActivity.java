70d835fb955fe559dac2addc27e7e99d86cceb96
Писарев Юрий <rarescrap@gmail.com>
2017-04-26 15:41:49 +0800

    Добавлены фрагменты и наведена красота в MainActivity

--- app/src/main/java/com/webtrust/tennosushi/MainActivity.java
+++ app/src/main/java/com/webtrust/tennosushi/MainActivity.java
@@ -6,16 +6,21 @@ import android.os.Bundle;
 
 import com.webtrust.tennosushi.fragments.MenuListFragment;
 
+/**
+ * Основная активити приложения
+ * @author RareScrap
+ */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Заполнение экрана начальным макетом
 
-        MenuListFragment startFragment = MenuListFragment.newInstance(MenuListFragment.PLATE_MODE);
+        // Создание экземпляра MenuListFragment и назначение ему режим отображения элементов в виде карточек
+        MenuListFragment startFragment = MenuListFragment.newInstance(MenuListFragment.CARD_MODE);
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.fragment_menu_container, startFragment);
-        transaction.commit(); // Отобразить фрагмент
+        transaction.add(R.id.fragment_menu_container, startFragment); // startFragment заменяет контейнер лдя фрагментов "fragment_menu_container"
+        transaction.commit(); // Завершить транзакцию и отобразить фрагмент
     }
 }
