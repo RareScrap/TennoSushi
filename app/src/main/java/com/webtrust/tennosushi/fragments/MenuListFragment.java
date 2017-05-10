d8fa33aa54350e19f47947e9b63ebcd81f57bee6
VladislavSavvateev <savvateevvlad@mail.ru>
2017-05-10 09:03:10 +0900

    Изменён путь до файла с JSON-контентом: теперь файл хранится на публичном сервере!

--- app/src/main/java/com/webtrust/tennosushi/fragments/MenuListFragment.java
+++ app/src/main/java/com/webtrust/tennosushi/fragments/MenuListFragment.java
@@ -161,7 +161,7 @@ public class MenuListFragment extends Fragment {
 
         // Запрос на получение данных
         try {
-            URL url = new URL("http://192.168.1.254/index.php");
+            URL url = new URL("http://romhacking.pw/sushi.php");
 
             GetDataTask getLocalDataTask = new GetDataTask();
             getLocalDataTask.execute(url);
