6fae30ea87716c559b48cb646c3a3930bee95fae
RareScrap <RareScrap@users.noreply.github.com>
2017-07-15 20:21:38 +0800

    Добавлена реализация облака тего (без дизайа)

--- app/src/main/java/com/webtrust/tennosushi/fragments/FoodListFragment.java
+++ app/src/main/java/com/webtrust/tennosushi/fragments/FoodListFragment.java
@@ -1,6 +1,5 @@
 package com.webtrust.tennosushi.fragments;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment; // Подлючается для использования в javadoc
@@ -12,18 +11,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button; // Для отслеживания кнопки "скрыть облако тегов"
 
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout; // Раскрываемый контейнер для облака тегов
 import com.webtrust.tennosushi.MainActivity;
 import com.webtrust.tennosushi.R;
 import com.webtrust.tennosushi.adapters.FoodItemRecyclerViewAdapter;
 import com.webtrust.tennosushi.list_items.FoodItem;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.webtrust.tennosushi.utils.FoodTag; // Для работы облака тегов
 
 import java.util.ArrayList;
 import java.util.List;
+
+import co.lujun.androidtagview.TagContainerLayout; // Облако тегов
+import co.lujun.androidtagview.TagView; // Для установки слушателя на тег
 
 /**
  * <p> Фрагмент, реализующий список блюд для какой либо категории. </p>
@@ -56,11 +57,22 @@ import java.util.List;
 public class FoodListFragment extends MenuListFragment {
     /** Список объектов FoodItem, представляющих элементы меню (блюда) */
     public List<FoodItem> foodItemList = new ArrayList<>();
+    /** Теги блюд для данной категории */
+    private ArrayList<FoodTag> tags = new ArrayList<>();
+    /** Теги, по которым пользователь сделал клик, добавив их тем самым в фильтр */
+    private ArrayList<FoodTag> checkedFoodTags = new ArrayList<>();
     /** Название категории меню */
     private String foodCategoryName;
 
     /** Элемент GUI, реализующий функции отображения списка */
     private RecyclerView recyclerView;
+    /** Элемент GUI, вызывающий сжатие/расширение облака тегов {@link #} */
+    private Button expandableButton;
+    /** Элемет GUI, представляющий собой котейнер для облака тегов */
+    private ExpandableRelativeLayout expandableLayout;
+    /** Элемент GUI, представляющий собой облако тегов */
+    private TagContainerLayout tagContainerLayout;
+
     // TODO: Нужно ли вообще сохранять ссылки на LayoutManager'ы? Пока сохраняю их "на всякий случай"
     /** Ссылка на последний исползуемый фрагментом LayoutManager для списка в виде карточек */
     private RecyclerView.LayoutManager listLayoutManager;
@@ -69,7 +81,7 @@ public class FoodListFragment extends MenuListFragment {
 
     /** Адаптер для связывания {@link FoodListFragment#recyclerView} c {@link FoodListFragment#listLayoutManager}
      * или {@link FoodListFragment#gridLayoutManager} (в зависимости от значения {@link MenuListFragment#currentMode}.*/
-    private FoodItemRecyclerViewAdapter rvAdapter; // Адаптер
+    private FoodItemRecyclerViewAdapter rvAdapter;
 
     /**
      * Необходимый пустой публичный конструктор.
@@ -134,10 +146,30 @@ public class FoodListFragment extends MenuListFragment {
         return inflater.inflate(R.layout.fragment_item_recyclerview_test, (ViewGroup) this.getView(), false);
     }
 
+    /**
+     * Вызывается после метода {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}. Получает
+     * ссылки на элементы GUI, устанавливает слушатели, определяет режим отображения списка,
+     * задает начальный вид списка (плитками или постами), устанавливает ему адаптер,
+     * назначает текст ActionBar'у, получает данные из {@link com.webtrust.tennosushi.DataProvider}
+     * и заполняет облако тегов.
+     * @param view {@link View} фрагмента, которое было создано до вызова этого метода
+     * @param savedInstanceState Обьект состояния фрагмента
+     */
     @Override
     public void onViewCreated (View view, Bundle savedInstanceState) {
-        // Получение ссылки на recyclerView
+        // Получение ссылки на элементы GUI
         recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView);
+        expandableButton = (Button) getView().findViewById(R.id.expandableButton);
+        expandableLayout = (ExpandableRelativeLayout) getActivity().findViewById(R.id.expandableLayout);
+        tagContainerLayout = (TagContainerLayout) getActivity().findViewById(R.id.tagcontainerLayout);
+
+        // TODO Вряд ли работает. Оставленно чтобы знать где ставить цвет тегам.
+        tagContainerLayout.setTagBackgroundColor(R.color.tag);
+        tagContainerLayout.setTagBorderColor(R.color.tag);
+
+        // Установка слушателей
+        expandableButton.setOnClickListener(expandableButtonClickListener);
+        tagContainerLayout.setOnTagClickListener(tagClickListener);
 
         /*
         Инициализировать LayoutManager для определенного вида списка.
@@ -154,7 +186,8 @@ public class FoodListFragment extends MenuListFragment {
         }
 
         // Создать RecyclerView.Adapter для связывания элементов списка foodItemList с RecyclerView
-        rvAdapter = new FoodItemRecyclerViewAdapter(foodItemList, itemClickListener, buyItemClickListener);
+        rvAdapter = new FoodItemRecyclerViewAdapter(foodItemList, itemClickListener,
+                buyItemClickListener/*, new ArrayList<FoodTag>()*/);
         recyclerView.setAdapter(rvAdapter);
 
         // Названичение текста actionBar'у
@@ -165,14 +198,38 @@ public class FoodListFragment extends MenuListFragment {
         // Получение данных из DataProvider
         try {
             ArrayList<FoodItem> downloadedFoodItemListLink = ((MainActivity) getActivity()).getDataProvider().downloadedFoodItemList;
-            for (int i = 0; i < downloadedFoodItemListLink.size(); ++i ) {
+            for (int i = 0; i < downloadedFoodItemListLink.size(); i++ ) {
                 if (downloadedFoodItemListLink.get(i).categoryId == getArguments().getInt("foodCategory"))
                     foodItemList.add(downloadedFoodItemListLink.get(i));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // Получение тегов из блюд для данной категории
+        // Ссылка на TagContainerLayout, в котором отображаются теги
+        for (int i = 0; i < foodItemList.size(); ++i) {
+            FoodItem foodItem = foodItemList.get(i);
+            for (int j = 0; j < foodItem.tags.size(); ++j) {
+                // Проверка на существование полученого тега в списке
+                if (!tags.contains(foodItem.tags.get(j)))
+                    tags.add(foodItem.tags.get(j));
+            }
+        }
+
+        // TODO: Ниже очень много закомментированного кода
+        // Добавление сформированного списка тегов в облако тегов
+        //List<String> tagsString = new ArrayList<>();
+        for (int i = 0; i < tags.size(); ++i) {
+            tagContainerLayout.addTag(tags.get(i).name, i);
+        }
+        //mTagContainerLayout.setTags(tags);
     }
+    //ExpandableRelativeLayout expandableLayout1;
+    /*public void expandableButton1(View view) {
+        expandableLayout1 = (ExpandableRelativeLayout) getActivity().findViewById(R.id.expandableLayout1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+    }*/
 
     /**
      * Обработка выбора команд меню.
@@ -259,6 +316,87 @@ public class FoodListFragment extends MenuListFragment {
 
             // Отобразать уведомление о добавлении
             Snackbar.make(getView(), "Добавлено в корзину ;)", Snackbar.LENGTH_SHORT).show();
+        }
+    };
+
+    /**
+     * Обрабатывает события нажатия на кнопку сокрытия/раскрытия облака тегов (вернее, его
+     * контейнера {@link #expandableLayout}
+     */
+    private /*TODO: final?*/ View.OnClickListener expandableButtonClickListener = new View.OnClickListener() {
+        // TODO: Javadoc не может сослаться на приватное поле
+        /**
+         * Вызываеся при клике а кнопку сокрытия/раскрытия контейера {@link #expandableLayout}
+         * облака тегов {@link #tagContainerLayout}
+         * @param v Кнопка, по которой был сделан клик
+         */
+        @Override
+        public void onClick(View v) {
+            expandableLayout.toggle();
+        }
+    };
+
+    /**
+     * Слушатель, обрабатывающий взаимодействия с тегами (например, клик по View тега)
+     */
+    private TagView.OnTagClickListener tagClickListener = new TagView.OnTagClickListener() {
+        /**
+         * Обрабатывает события нажатия на View тега, добавляя его тем самым в фильтр. Если было
+         * добавлено несколько тегов - будут показаны только те товары, которые содержат все эти теги
+         */
+        @Override
+        public void onTagClick(int position, String text) {
+            // Получаем FoodTag, соответвтсвующий вьюхе тега по которой был сделан клик
+            FoodTag foodTag = tags.get(position);
+
+            // Если тега еще нет с списке выбранных - добавляем его туда
+            if (!checkedFoodTags.contains(foodTag)) {
+                checkedFoodTags.add( tags.get(position) );
+                tagContainerLayout.getTagView(position).setTagBackgroundColor(getResources().getColor(R.color.checkedTag));
+
+                // TODO: Закоментированный код в каждом из if - способы задаия цвета тегу. Некоторые из их не работают, а некоторые работают специфисно, но интересно. Нужно расписать что выходит на каждой строке и сделать из этого гист
+                //tagContainerLayout.getTagView(position).setBackgroundColor(getResources().getColor(R.color.checkedTag));
+                //tagContainerLayout.setTagBackgroundColor(getResources().getColor(R.color.checkedTag));
+                //tagContainerLayout.setTagBackgroundColor(Color.GREEN);
+            } else { // Иначе - удаляем тег из выбранных, т.к. это значит что он уже был выбран пользователем
+                checkedFoodTags.remove(foodTag);
+                tagContainerLayout.getTagView(position).setTagBackgroundColor(getResources().getColor(R.color.tag));
+
+                //tagContainerLayout.getTagView(position).setBackgroundColor(getResources().getColor(R.color.tag));
+                //tagContainerLayout.setTagBackgroundColor(getResources().getColor(R.color.tag));
+                //tagContainerLayout.setTagBackgroundColor(Color.BLACK);
+                //tagContainerLayout.setTheme(ColorFactory.NONE);
+            }
+
+            // Очищаем список отображаемых товаров и заполяем его всеми товарами, подходящими под данную категорию блюд
+            foodItemList.clear();
+            // Создаем копию списка всех (ВООБЩЕ ВСЕХ) загруженных товаров
+            ArrayList<FoodItem> downloadedFoodItemListLink = ((MainActivity) getActivity()).getDataProvider().downloadedFoodItemList;
+            // Формируем список блюд, прошедших фильтрацию
+            for (int i = 0; i < downloadedFoodItemListLink.size(); i++ ) {
+                if (downloadedFoodItemListLink.get(i).categoryId == getArguments().getInt("foodCategory"))
+                    foodItemList.add(downloadedFoodItemListLink.get(i));
+            }
+            rvAdapter.items = foodItemList; // Отображаем товары на экране
+
+            // Формируем список блюд, прошедших фильтрацию
+            for (int i = 0; i < rvAdapter.items.size() && !checkedFoodTags.isEmpty() ; i++) {
+                if (!rvAdapter.items.get(i).tags.containsAll(checkedFoodTags))
+                    rvAdapter.items.remove(i);
+            }
+
+            // Измещаем адаптер, об изменеии его данных
+            rvAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onTagLongClick(int position, String text) {
+
+        }
+
+        @Override
+        public void onTagCrossClick(int position) {
+
         }
     };
 }
